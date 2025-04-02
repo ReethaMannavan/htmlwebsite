92f51712545b45a707ae954b5bf53cb693c19cd4
ReethaMannavan <108715512+ReethaMannavan@users.noreply.github.com>
2025-04-02 20:58:31 +0300

    Update script65.js

--- Day5/assets/js/script65.js
+++ Day5/assets/js/script65.js
@@ -1,1 +1,12 @@
-// 
+function greet(name, callback) {
+    console.log('Hi' + ' ' + name);
+    callback();
+}
+
+
+function callMe() {
+    console.log('I am callback function');
+}
+
+
+greet('john', callMe);
