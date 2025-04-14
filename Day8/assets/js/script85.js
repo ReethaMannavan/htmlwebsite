0b4c682355d0b9c22a60d614189064b345946320
ReethaMannavan <108715512+ReethaMannavan@users.noreply.github.com>
2025-04-14 23:35:06 +0300

    Delete Day8 directory

--- Day8/assets/js/script85.js
+++
@@ -1,8 +0,0 @@
-// JS85. Implement default values in a constructor function when no parameters are provided. 
-
-function User() { 
-    this.name = "admin"; 
-} 
- 
-let adminUser = new User(); 
-console.log(adminUser.name); 
